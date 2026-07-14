e0df09bc6a7616817ed0c3c25e84fb2144849c0a
Cma-13 <aasmasingdan77@gmail.com>
2026-07-14 16:06:03 +0545

    feat: prepare place order data to send to backend, db tables config for order and orderitems, implement place order logic in view

--- orders/admin.py
+++ orders/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
-from .models import Table, Category, MenuItem
+from .models import Table, Category, MenuItem, Order, OrderItem
 
 
 @admin.register(Table)
 class TableAdmin(admin.ModelAdmin):
     list_display = ['name', 'is_reserved']
-    # list_filter = ('is_reserved',
-    # search_fields = ('name',)
+
     
 
 admin.site.register(Category)
 admin.site.register(MenuItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
--- orders/views.py
+++ orders/views.py
@@ -1,11 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .decorators import role_required
 from accounts.models import User
-from .models import Table
+from .models import Category, Table, Order, OrderItem, MenuItem
+import json
+from django.contrib import messages 
 @role_required([User.ROLE_CHOICES.WAITER])
 def tables_view(request):
     tables = Table.objects.all()
     
     return render(request, 'orders/tables.html',{
         'tables': tables
+    })
+    
+    
+@role_required([User.ROLE_CHOICES.WAITER])
+def menu_view(request, table_id):
+    
+    if request.method == 'POST':
+        data = request.POST.get("order-items")
+        data_dict = json.loads(data)
+        table_id = data_dict.get("table_id")
+        table_obj = Table.objects.get(pk=table_id)
+        order = Order.objects.create(table=table_obj)
+        
+        
+        for orderitem in data_dict.get("items"):
+            menu_item = MenuItem.objects.get(pk=orderitem.get("item_id"))
+            OrderItem.objects.create(
+                order=order,
+                menu_item=menu_item,
+                price = menu_item.price,
+                quantity = orderitem.get("quantity")
+            )
+        messages.success(request, 'Order created successfully.')
+            
+         
+        
+        return redirect("tables_view_url")
+    
+    categories = Category.objects.all()
+    
+    return render(request, 'orders/menu.html',{
+        'categories': categories,
+        'table_id': table_id    
     })
